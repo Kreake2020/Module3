1fb033734708a32aa740eb4d1a6f9a0a0dc04881
Thomas Riddle <tmr0042@auburn.edu>
2025-02-02 07:39:04 -0600

    Initial Commit

---
+++ Module3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Module3
+//
+//  Created by user268065 on 2/2/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Module3Tests/Module3Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Module3Tests.swift
+//  Module3Tests
+//
+//  Created by user268065 on 2/2/25.
+//
+
+import Testing
+@testable import Module3
+
+struct Module3Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
